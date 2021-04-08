b06f60e1ae52d024e087294d416458c08c965146
Facundo Cristaldo <facu.cristaldo.96@gmail.com>
2021-04-08 12:34:52 -0300

    change

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/', function (req, res) {
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example aapp listening at http://%s:%s', host, port);
 });
